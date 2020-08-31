9e088b074cff6cb2b9b72f933ce075932cdb0245
NiteenRC <niteen2010@gmail.com>
2020-08-31 17:16:34 +0530

    Reverse given string, each word and Number

---
+++ src/test/java/com/nc/coding/string/ReverseNumStringTest.java
@@ -0,0 +1,45 @@
+package com.nc.coding.string;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ReverseNumStringTest {
+	@InjectMocks
+	private ReverseNumString reverseString;
+
+	@Test
+	public void reverseByStringConcatTest() {
+		String reverseRes = reverseString.reverseByStringConcat("abcd");
+		assertEquals("dcba", reverseRes);
+	}
+
+	@Test
+	public void reverseByStringBuilderTest() {
+		String reverseRes = reverseString.reverseByStringBuilder("abcd");
+		assertEquals("dcba", reverseRes);
+	}
+
+	@Test
+	public void reverseStringRecursivelyTest() {
+		String reverseRes = reverseString.reverseByRecursively("abcd");
+		assertEquals("dcba", reverseRes);
+	}
+
+	@Test
+	public void reverseEachStringByStringConcatTest() {
+		String reverseRes = reverseString.reverseEachStringByStringConcat("abcd bdsa");
+		System.out.println(reverseRes);
+		assertEquals("dcba asdb", reverseRes);
+	}
+
+	@Test
+	public void reverseNumberTest() {
+		int reverseRes = reverseString.reverseNumber(1234);
+		assertEquals(4321, reverseRes);
+	}
+}
